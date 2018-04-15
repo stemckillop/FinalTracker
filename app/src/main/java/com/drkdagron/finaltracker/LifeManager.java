8ca0d9106b1034f29dbeaaf1a316679d9f789f7c
stemc <ste.mckillop@gmail.com>
2018-04-15 10:03:12 -0400

    2P Life complete

---
+++ app/src/main/java/com/drkdagron/finaltracker/LifeManager.java
@@ -0,0 +1,51 @@
+package com.drkdagron.finaltracker;
+
+public class LifeManager {
+    static LifeManager lifeManager;
+    public static LifeManager getInstance() {
+        if (lifeManager == null)
+            lifeManager = new LifeManager();
+
+        return lifeManager;
+    }
+
+    class Player {
+        public String Name;
+        public int Life;
+
+        public Player() {
+            Life = 20;
+            Name = "Player";
+        }
+    }
+    public Player[] players;
+
+    public LifeManager() {
+        players = new Player[2];
+        resetPlayers();
+    }
+
+    public LifeManager(int p) {
+        players = new Player[p];
+        resetPlayers();
+    }
+
+    /*
+    /This function will reset the players lives and names (usually used if going from 3 players -> 4 players
+    */
+    public void resetPlayers() {
+        for (int i =0; i < players.length; i++) {
+            players[i] = new Player();
+        }
+    }
+
+    /*
+    /This function will reset the players lives but not touch any names they may have chosen (used for game resets)
+    */
+    public void restartPlayers() {
+        for (int i =0; i < players.length; i++) {
+            players[i].Life = 20;
+        }
+    }
+
+}
--- app/src/main/java/com/drkdagron/finaltracker/P2Activity.java
+++ app/src/main/java/com/drkdagron/finaltracker/P2Activity.java
@@ -2,13 +2,17 @@ package com.drkdagron.finaltracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 public class P2Activity extends AppCompatActivity {
+    public final String TAG = "P2Activity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        LifeManager.getInstance();
         setContentView(R.layout.activity_p2);
     }
 
@@ -17,6 +21,24 @@ public class P2Activity extends AppCompatActivity {
         super.onResume();
 
         fixWindow();
+    }
+
+    public void modifyLife(View v) {
+        //Log.w(TAG, v.getTag().toString().substring(3));
+        int player = Integer.parseInt(v.getTag().toString().substring(1, 2));
+        String action = v.getTag().toString().substring(3);
+
+        if (action.equals("add")) {
+            LifeManager.getInstance().players[player-1].Life++;
+        } else if (action.equals("sub")) {
+            LifeManager.getInstance().players[player-1].Life--;
+        }
+
+        if (player == 1) {
+            ((TextView)findViewById(R.id.p1L)).setText(String.valueOf(LifeManager.getInstance().players[player-1].Life));
+        } else if (player == 2) {
+            ((TextView)findViewById(R.id.p2L)).setText(String.valueOf(LifeManager.getInstance().players[player-1].Life));
+        }
     }
 
     private void fixWindow() {
